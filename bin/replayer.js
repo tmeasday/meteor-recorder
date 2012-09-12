839badca6f3ef1752ebadfd09ba287a36ee18c67
Tom Coleman <tom@thesnail.org>
2012-09-12 17:28:14 +1000

    Hack to make phantom exit.

--- bin/replayer.js
+++ bin/replayer.js
@@ -5,20 +5,24 @@ var replay = system.args[1];
 
 if (!replay) {
   console.log('Please provide a name of a replay to run');
-  Phantom.exit();
+  phantom.exit();
 }
 
 
 page.onConsoleMessage = function (msg) { console.log(msg); };
+// there must be a better way to do this?
+page.onAlert = function(msg) {
+  console.log('Replay finished. Goodnight and good luck.');
+  msg === 'replayer-is-finished' && phantom.exit();
+}
 
 page.open('http://localhost:3000', function(status) {
   var result = page.evaluate(function(replay) {
     Meteor.deps.await_once(function() { return Recorder.ready(); }, function() {
       Recorder.replay(replay, function(err) {
-        console.log(err);
-        console.log('done');
+        err & console.log(err);
+        alert('replayer-is-finished');
       });
     });
   }, replay);
-  // Phantom.exit();
 });
